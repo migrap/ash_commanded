35ff4b23ed8c5d6c4f016cfcad9cd93b6fc68cf9
Michael Park <migrap@gmail.com>
2025-07-18 08:28:48 -0700

    deps ash-project/spark

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule AshCommanded.MixProject do
     [
       {:ash, "~> 3.0"},
       {:sourceror, "~> 1.8", only: [:dev, :test]},
-      {:spark, "~> 2.2"},
+      {:spark, github: "ash-project/spark", branch: "main", override: true},
       {:igniter, "~> 0.5", only: [:dev, :test]},
       {:mock, "~> 0.3.0", only: [:test]},
 
